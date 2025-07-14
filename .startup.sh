5a9fde6c5e5a9c1d917031773e0d64d665eeb51c
William Ross <contact@williamross.dev>
2025-07-14 07:13:35 -0500

    Added startup script for installing Homebrew on Mac and initializing Chezmoi

---
+++ .startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin"]; then
+    if which -s brew then
+        echo 'Homebrew is already installed'
+    else
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+
+    brew install chezmoi
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    snap install chezmoi --classic
+else
+    echo 'Operating system not supported'
+fi
+
+chezmoi init william-ross
+chezmoi apply
